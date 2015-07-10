2f3798e693d86f060f0c1c31f4cae490a7d5ec63
Hideaki Ohno <hide.o.j55@gmail.com>
2015-07-10 17:28:43 +0900

    Add test pattern

--- waveletmatrix_test.go
+++ waveletmatrix_test.go
@@ -20,26 +20,132 @@ func TestBuildAndAccess(t *testing.T) {
 			t.Error("Expected", src[i], "Got", v)
 		}
 	}
-	if r, _ := wm.Rank(uint64(3), uint64(6)); r != uint64(0) {
+	if r, _ := wm.Rank(3, 6); r != uint64(0) {
 		t.Error("Expected", 0, "Got", r)
 	}
-	if r, _ := wm.Rank(uint64(0), uint64(6)); r != uint64(1) {
+	if r, _ := wm.Rank(0, 6); r != uint64(1) {
 		t.Error("Expected", 1, "Got", r)
 	}
-	if r, _ := wm.Rank(uint64(0), uint64(7)); r != uint64(2) {
+	if r, _ := wm.Rank(0, 7); r != uint64(2) {
 		t.Error("Expected", 2, "Got", r)
 	}
-	if r, _ := wm.Rank(uint64(2), uint64(6)); r != uint64(2) {
+	if r, _ := wm.Rank(2, 6); r != uint64(2) {
 		t.Error("Expected", 2, "Got", r)
 	}
-	if r, _ := wm.Rank(uint64(5), uint64(6)); r != uint64(1) {
+	if r, _ := wm.Rank(5, 6); r != uint64(1) {
 		t.Error("Expected", 1, "Got", r)
 	}
-	if _, found := wm.Rank(uint64(1), uint64(10)); found {
+	if _, found := wm.Rank(1, 10); found {
 		t.Error("Expected", false, "Got", found)
 	}
-	if pos, _ := wm.Select(uint64(2), uint64(1)); pos != uint64(4) {
-		t.Error("Expected", 2, "Got", pos)
-
+	if pos, _ := wm.Select(2, 1); pos != uint64(4) {
+		t.Error("Expected", 4, "Got", pos)
 	}
+
+	if pos, _ := wm.Select(2, 2); pos != uint64(5) {
+		t.Error("Expected", 5, "Got", pos)
+	}
+
+	if r := wm.RankLessThan(4, 5); r != uint64(3) {
+		t.Error("Expected", 3, "Got", r)
+	}
+	if r := wm.RankMoreThan(3, 5); r != uint64(2) {
+		t.Error("Expected", 2, "Got", r)
+	}
+	if f := wm.Freq(2); f != uint64(2) {
+		t.Error("Expected", 2, "Got", f)
+	}
+	if f := wm.FreqRange(2, 5, 2, 6); f != uint64(3) {
+		t.Error("Expected", 3, "Got", f)
+	}
+	if f := wm.FreqSum(0, 3); f != uint64(5) {
+		t.Error("Expected", 5, "Got", f)
+	}
+
+	pos, val := wm.MaxRange(1, 6)
+	if pos != uint64(3) {
+		t.Error("Expected", 3, "Got", pos)
+	}
+	if val != uint64(4) {
+		t.Error("Expected", 4, "Got", val)
+	}
+
+	pos, val = wm.MinRange(1, 6)
+	if pos != uint64(2) {
+		t.Error("Expected", 2, "Got", pos)
+	}
+	if val != uint64(0) {
+		t.Error("Expected", 0, "Got", val)
+	}
+
+	pos, val = wm.QuantileRange(1, 6, 3)
+	if pos != uint64(5) {
+		t.Error("Expected", 5, "Got", pos)
+	}
+	if val != uint64(2) {
+		t.Error("Expected", 2, "Got", val)
+	}
+
+	result := wm.ListModeRange(1, 3, 0, 8, 3)
+	if size := len(result); size != 2 {
+		t.Error("Expected", 2, "Got", size)
+	}
+	if result[0].C != uint64(2) {
+		t.Error("Expected", 2, "Got", result[0].C)
+	}
+	if result[0].Freq != uint64(2) {
+		t.Error("Expected", 2, "Got", result[0].Freq)
+	}
+	if result[1].C != uint64(1) {
+		t.Error("Expected", 1, "Got", result[1].C)
+	}
+	if result[1].Freq != uint64(1) {
+		t.Error("Expected", 1, "Got", result[1].Freq)
+	}
+
+	result = wm.ListMaxRange(1, 5, 0, 8, 3)
+	if size := len(result); size != 3 {
+		t.Error("Expected", 3, "Got", size)
+	}
+	if result[0].C != uint64(4) {
+		t.Error("Expected", 4, "Got", result[0].C)
+	}
+	if result[0].Freq != uint64(1) {
+		t.Error("Expected", 1, "Got", result[0].Freq)
+	}
+	if result[1].C != uint64(3) {
+		t.Error("Expected", 3, "Got", result[1].C)
+	}
+	if result[1].Freq != uint64(1) {
+		t.Error("Expected", 1, "Got", result[1].Freq)
+	}
+	if result[2].C != uint64(2) {
+		t.Error("Expected", 2, "Got", result[2].C)
+	}
+	if result[2].Freq != uint64(2) {
+		t.Error("Expected", 2, "Got", result[2].Freq)
+	}
+	result = wm.ListMinRange(0, 5, 0, 8, 3)
+	if size := len(result); size != 3 {
+		t.Error("Expected", 3, "Got", size)
+	}
+	if result[0].C != uint64(0) {
+		t.Error("Expected", 0, "Got", result[0].C)
+	}
+	if result[0].Freq != uint64(2) {
+		t.Error("Expected", 2, "Got", result[0].Freq)
+	}
+	if result[1].C != uint64(1) {
+		t.Error("Expected", 1, "Got", result[1].C)
+	}
+	if result[1].Freq != uint64(1) {
+		t.Error("Expected", 1, "Got", result[1].Freq)
+	}
+	if result[2].C != uint64(2) {
+		t.Error("Expected", 0, "Got", result[2].C)
+	}
+	if result[2].Freq != uint64(2) {
+		t.Error("Expected", 2, "Got", result[2].Freq)
+	}
+
 }

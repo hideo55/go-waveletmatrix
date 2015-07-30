64f2186f02ee17a5770d838b2f9d360a512e5db4
hideo55 <hide.o.j55@gmail.com>
2015-07-30 23:41:34 +0900

    Update godoc(Add Synopsis)

--- waveletmatrix.go
+++ waveletmatrix.go
@@ -1,5 +1,30 @@
 /*
 Package waveletmatrix is implementation of Wavelet-Matrix in Go.
+
+Synopsis
+
+	package main
+
+	import (
+		"fmt"
+
+		"github.com/hideo55/go-waveletmatrix"
+	)
+
+	func main() {
+		src := []uint64{1, 3, 1, 4, 2, 1, 10}
+		wm, err := waveletmatrix.NewWM(src)
+		if err != nil {
+			// Failed to build wavelet-matrix
+		}
+		val, _ := wm.Lookup(3)
+		fmt.Println(val) // 4 ... src[3]
+		rank, _ := wm.Rank(2, 6)
+		fmt.Println(rank) // 1 ... The number of 2 in src[0..5]
+		pos, _ := wm.Select(1, 3) // = 5 ... The third 1 appeared in src[5]
+		fmt.Println(pos)
+	}
+
 */
 package waveletmatrix
 
